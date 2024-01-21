9e4c7ce5299fdecd35432a8ebbcd01080777f7da
adityachopkar2907 <adityachopakar@gmail.com>
2024-01-21 11:20:42 +0530

    New action method added in demo controller

--- SchoolSystem/Controllers/DemoController.cs
+++ SchoolSystem/Controllers/DemoController.cs
@@ -13,5 +13,14 @@ namespace SchoolSystem.Controllers
         {
             return View();
         }
+
+        public ActionResult Index1()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return View();
+        }
     }
 }
