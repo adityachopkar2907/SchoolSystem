fe33ba57c73b86d9a1696d04eea2a59ad52eb803
adityachopkar2907 <adityachopakar@gmail.com>
2024-01-21 11:34:14 +0530

    Control is succesfully loaded

---
+++ SchoolSystem/Controllers/home1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolSystem.Controllers
+{
+    public class home1Controller : Controller
+    {
+        // GET: home1
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
